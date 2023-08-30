2574eac0ac1cf6b5f869b3ab4a607b7f31fd0a35
walaa1422 <142272795+walaa1422@users.noreply.github.com>
2023-08-30 23:28:21 +0300

    new

--- sizedataset.py
+++ sizedataset.py
@@ -2,9 +2,9 @@
 from PIL import Image
 import os
 
-input_folder = 'C:/Users/96655/Desktop/mm'
-output_folder = 'C:/Users/96655/Desktop/mm'
-target_size = (200, 200)  # حجم الصور المستهدف
+input_folder = "C:/Users/96655/Desktop/mm/test"
+output_folder = "C:/Users/96655/Desktop/mm/New folder"
+target_size = (100, 100)  # حجم الصور المستهدف
 
 os.makedirs(output_folder, exist_ok=True)
 
@@ -12,6 +12,6 @@ for filename in os.listdir(input_folder):
     if filename.endswith(".jpg"):  # افترض أن الصور بتنسيق JPG
         img_path = os.path.join(input_folder, filename)
         img = Image.open(img_path)
-        resized_img = img.resize(target_size, Image.ANTIALIAS)
+        resized_img = img.resize(target_size, Image.ANTIALIAS)  # استخدام Image.ANTIALIAS هنا
         output_path = os.path.join(output_folder, filename)
         resized_img.save(output_path, quality=95)  # يمكن تعديل جودة الصورة حسب الحاجة
