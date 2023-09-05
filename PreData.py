4e312f4bb10153d30a4b3af992561699e412c1ae
walaa1422 <142272795+walaa1422@users.noreply.github.com>
2023-09-05 20:23:51 +0300

    يي

--- PreData.py
+++ PreData.py
@@ -1,59 +1,38 @@
-#الكود كامل لعملية معالجة الصور - شغال 
-from PIL import Image, ImageEnhance
-import imagehash #pip install imagehash 
+import cv2
 import os
 
-def enhance_image_quality(image):
-    enhancer = ImageEnhance.Contrast(image)
-    image = enhancer.enhance(1)  # زيادة التباين بنسبة 200%, اذا كان 1 م راح يفرق نفس الاصلي
-    return image
+# دالة لتحسين الجودة وتغيير حجم الصور وتطبيق تصفية الصورة
+def process_image(image):
+    # تحسين الجودة
+    enhanced_image = cv2.equalizeHist(image)
 
-def remove_duplicate_images(images):#تُستخدم لإزالة الصور المكررة من مجموعة الصور
-    image_hashes = []
-    unique_images = []
+    # تغيير حجم الصورة
+    resized_image = cv2.resize(enhanced_image, (224, 224))
 
-    for img in images:
-        img_hash = str(imagehash.phash(img))
+    # تطبيق تصفية الصورة
+    filtered_image = cv2.GaussianBlur(resized_image, (5, 5), 0)  # مثال باستخدام Gaussian Blur
 
-        if img_hash not in image_hashes:
-            image_hashes.append(img_hash)
-            unique_images.append(img)
+    return filtered_image
 
-    return unique_images
+# مجلد المصدر للصور الشعاعية
+source_folder = 'مسار_مجلد_المصدر'
 
-def preprocess_images(images):
-    processed_images = []
-    for img in images:
-        # تغيير حجم الصورة إلى 200x200 بكسل
-        img = img.resize((224,224))
+# مجلد الوجهة للصور المعالجة
+destination_folder = 'مسار_مجلد_الوجهة'
 
-        # تحسين الجودة
-        img = enhance_image_quality(img)
+# قراءة الصور من المجلد المصدر
+image_files = [f for f in os.listdir(source_folder) if f.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
 
-        processed_images.append(img)
-    return processed_images
+# معالجة الصور وحفظها في مجلد الوجهة
+for filename in image_files:
+    image_path = os.path.join(source_folder, filename)
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # اقرأ الصورة كصورة رمادية
 
-if __name__ == "__main__":
-    source_folder = 'C:/Users/96655/Desktop/bige data/train/fractured'
-    target_folder = 'C:/Users/96655/Desktop/preimaeg/tr1'
+    # معالجة الصورة
+    processed_image = process_image(image)
 
-    # تحميل الصور من المجلد المصدر
-    images = [Image.open(os.path.join(source_folder, filename)) for filename in os.listdir(source_folder)]
+    # حفظ الصور المعالجة في مجلد الوجهة
+    destination_path = os.path.join(destination_folder, filename)
+    cv2.imwrite(destination_path, processed_image)
 
-    # تحسين جودة الصور
-    enhanced_images = [enhance_image_quality(img) for img in images]
-
-    # إزالة الصور المكررة
-    unique_images = remove_duplicate_images(enhanced_images)
-
-    # تطبيق عمليات معالجة الصور
-    processed_images = preprocess_images(unique_images)
-
-    # حفظ الصور المعالجة في المجلد المستهدف
-    os.makedirs(target_folder, exist_ok=True)
-    for i, img in enumerate(processed_images):
-        image_name = f'processed_image_{i}.jpg'
-        img.save(os.path.join(target_folder, image_name))
-
-    print(f'تم تحسين جودة وتغيير حجم {len(processed_images)} صورة وحفظها في المجلد المستهدف.')
-
+print(f'تم معالجة وتصفية {len(image_files)} صورة وحفظها في مجلد الوجهة.')
