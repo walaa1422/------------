cc7fda052947f273392b87e695dd081b3d6d0284
walaa1422 <142272795+walaa1422@users.noreply.github.com>
2023-09-09 18:13:39 +0300

    w

--- delet-rotate.py
+++ delet-rotate.py
@@ -6,17 +6,17 @@ def delete_images_with_name(folder_path, target_name):
     image_files = os.listdir(folder_path)
 
     # حذف الصور التي تحتوي على الاسم المستهدف
+    
     for image_file in image_files:
         if target_name in image_file:
             image_path = os.path.join(folder_path, image_file)
             os.remove(image_path)
-            print(f"تم حذف الصورة: {image_file}")
-
-# تحديد المجلد الذي يحتوي على الصور
-folder_path = "C:/Users/n67-m/Downloads/not fractured-v"
-
-# تحديد الاسم المستهدف للصور المراد حذفها
+            print(f" Deleted successfully : {image_file}")
+            
+ #Specify the target name of the images to be deleted
 target_name = "rotated"
 
+#تغير اسم الباث
+folder_path = "C:/Users/n67-m/Downloads/not fractured-v"
 # حذف الصور التي تحتوي على الاسم المستهدف
 delete_images_with_name(folder_path, target_name)
