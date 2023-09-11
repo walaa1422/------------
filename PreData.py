b587d8d8d433a46f12b51c1166d5974475a03be3
walaa1422 <142272795+walaa1422@users.noreply.github.com>
2023-09-11 20:44:29 +0300

    s

--- PreData.py
+++ PreData.py
@@ -1,41 +1,41 @@
-import cv2
+from PIL import Image
 import os
+import cv2
+
+# المجلد الذي يحتوي على الصور الأصلية
+input_folder = "C:/Users/96655/Desktop/Datasetsplitter/testing/Fractions"
+# المجلد الذي سيتم حفظ الصور المصغرة فيه
+output_folder = "C:/Users/96655/Desktop/New folder"
+# الحجم الجديد المطلوب للصور المصغرة
+new_size = (224, 224)  # (العرض, الارتفاع)
 
 # دالة لتحسين الجودة وتغيير حجم الصور وتطبيق تصفية الصورة
-
 def process_image(image):
-
-    
-    #Improve quality
+    # Improve quality
     enhanced_image = cv2.equalizeHist(image)
-    #Resize the image
-    resized_image = cv2.resize(enhanced_image, (224, 224))
-    #Apply image filter
-    filtered_image = cv2.GaussianBlur(resized_image, (5, 5), 0) 
-
-
+    # Resize the image
+    resized_image = cv2.resize(enhanced_image, new_size)
+    # Apply image filter
+    filtered_image = cv2.GaussianBlur(resized_image, (5, 5), 0)
     return filtered_image
 
+# التأكد من وجود مجلد الإخراج وإنشائه إذا لم يكن موجودًا
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-# مجلد المصدر للصور الشعاعية
-source_folder = 'مسار_مجلد_المصدر'
+# قائمة بجميع ملفات الصور في المجلد المصدر
+image_files = [f for f in os.listdir(input_folder) if f.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
 
-# مجلد الوجهة للصور المعالجة
-destination_folder = 'مسار_مجلد_الوجهة'
-
-# قراءة الصور من المجلد المصدر
-image_files = [f for f in os.listdir(source_folder) if f.endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
-
-# معالجة الصور وحفظها في مجلد الوجهة
-for filename in image_files:
-    image_path = os.path.join(source_folder, filename)
+for image_file in image_files:
+    # فتح الصورة
+    image_path = os.path.join(input_folder, image_file)
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # اقرأ الصورة كصورة رمادية
 
     # معالجة الصورة
     processed_image = process_image(image)
 
-    # حفظ الصور المعالجة في مجلد الوجهة
-    destination_path = os.path.join(destination_folder, filename)
-    cv2.imwrite(destination_path, processed_image)
+    # حفظ الصور المعالجة في مجلد الإخراج
+    output_path = os.path.join(output_folder, image_file)
+    cv2.imwrite(output_path, processed_image)
 
-print(f'تم معالجة وتصفية {len(image_files)} صورة وحفظها في مجلد الوجهة.')
+print(f'تم معالجة وتغيير حجم {len(image_files)} صورة وحفظها في مجلد الإخراج.')
