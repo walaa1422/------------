4b442c04c9f3aff6f950f1c03d5df1c7482f862d
walaa1422 <142272795+walaa1422@users.noreply.github.com>
2023-10-14 02:02:28 +0300

    S

--- ResNet50.py
+++ ResNet50.py
@@ -9,45 +9,43 @@ from keras.optimizers import Adam
 from sklearn.utils.class_weight import compute_class_weight 
 from keras.layers import Dropout
 
-# تعيين مسارات المجلدين 
+# Set the paths of the two folders
 train_data_dir = 'C:/Users/96655/Downloads/Telegram Desktop/data-complete1/data-complete1/trainN'
 test_data_dir = 'C:/Users/96655/Downloads/Telegram Desktop/data-complete1/data-complete1/teastT'
-# تحديد حجم الصور والدُفعات 
+
+# Define the size of images and batches
 image_size = (224, 224) 
 batch_size = 32 
  
 # Load the ResNet50 model with pre-trained weights 
 base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3)) 
- 
 x = base_model.output 
 x = GlobalAveragePooling2D()(x) 
-x = BatchNormalization()(x)  # إضافة Batch Normalization هنا 
+x = BatchNormalization()(x)   
 x = Dense(128, activation='relu')(x) 
 x = Dropout(0.5)(x) 
 predictions = Dense(2, activation='softmax')(x) 
  
-# بناء النموذج النهائي 
+# Build the final model
 model = keras.Model(inputs=base_model.input, outputs=predictions) 
  
-# تجميع النموذج 
+# Assemble the model
 optimizer = Adam(learning_rate=0.0001)  # ضبط معدل التعلم 
 model.compile(optimizer=optimizer, 
               loss='sparse_categorical_crossentropy', 
               metrics=['accuracy']) 
  
-# تحسين توزيع الوزنات لمعالجة البيانات غير المتوازنة 
+# Improve weight distribution to handle imbalanced data
 train_datagen = ImageDataGenerator( 
     rescale=1./255, 
     shear_range=0.2, 
     zoom_range=0.2, 
-    horizontal_flip=True) 
- 
+    horizontal_flip=True)  
 train_generator = train_datagen.flow_from_directory( 
     train_data_dir, 
     target_size=image_size, 
     batch_size=batch_size, 
     class_mode='sparse') 
- 
 test_datagen = ImageDataGenerator(rescale=1./255) 
 test_generator = test_datagen.flow_from_directory( 
     test_data_dir, 
@@ -55,11 +53,9 @@ test_generator = test_datagen.flow_from_directory(
     batch_size=batch_size, 
     class_mode='sparse') 
  
-# تحسين النموذج باستخدام التحسين على مجموعة التدريب 
+# Improve the model using optimization on the training set
 model.fit(train_generator, epochs=10, validation_data=test_generator) 
- 
-# تقييم النموذج وحساب الدقة 
+# Define accuracy as a measure of performance
 test_loss, test_accuracy = model.evaluate(test_generator) 
 print(f'Test accuracy:: {test_accuracy * 100:.2f}%')
- 
 model.save('C:/Users/96655/Downloads/ResNet_model-allData-acc.h5')
