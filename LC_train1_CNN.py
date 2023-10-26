89277e62ceed9e90a77a14339b8e64612ea54155
Shooody89 <142966916+Shooody89@users.noreply.github.com>
2023-10-26 23:38:54 +0300

    hg

--- LC_train1_CNN.py
+++ LC_train1_CNN.py
@@ -4,15 +4,15 @@ from tensorflow import keras
 from keras import layers
 from keras.preprocessing.image import ImageDataGenerator
 
-# تعيين مسارات المجلدين
+# Setting Folder Paths
 train_data_dir = "C:/Users/SHAHAD/Downloads/Telegram Desktop/data-part/data-part/data1/train1"
 test_data_dir = "C:/Users/SHAHAD/Downloads/Telegram Desktop/data-part/data-part/data1/test1"
 
-# تحديد حجم الصور والدُفعات
-image_size = (224, 224)  # هذا ممكن نحذفه
+# Determining Image and Batch Sizes
+image_size = (224, 224) 
 batch_size = 32
 
-# تحديد العمارة المعمارية للنموذج CNN
+# Determining the Architectural Design of the CNN Model
 model = keras.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
     layers.MaxPooling2D((2, 2)),
@@ -22,44 +22,44 @@ model = keras.Sequential([
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
-    layers.Dense(2, activation='softmax')  # 2 فئات: مكسورة وسليمة
+    layers.Dense(2, activation='softmax')  #2 Categories: fracture and healthy bones
 ])
 
-# تحديد الدقة كمقياس للأداء
+# Defining Accuracy as a Performance Metric
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# إعداد توليفة البيانات للتحسين وزيادة البيانات
+# Preparing Data Augmentation for Improvement and Data Increase
 train_datagen = ImageDataGenerator(
-    rescale=1./255,  # تسوية قيم البكسل إلى مدى [0, 1]
-    shear_range=0.2,  # تدوير الصور بزاوية
-    zoom_range=0.2,  # تكبير/تصغير الصور
-    horizontal_flip=True)  # عكس الصور أفقيًا
+    rescale=1./255,  # Normalizing Pixel Values to the Range [0, 1]
+    shear_range=0.2,  # Rotating Images at an Angle
+    zoom_range=0.2,  # Scaling Images Up/Down
+    horizontal_flip=True)  # Flipping Images Horizontally
 
-# تحميل البيانات وتحضيرها
+# Data Loading and Preparation
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=image_size,
     batch_size=batch_size,
-    class_mode='sparse')  # تحديد الفئات على أنها sparse
+    class_mode='sparse')  # Defining Categories as Sparse
 
-test_datagen = ImageDataGenerator(rescale=1./255)  # تسوية قيم البكسل لمجموعة الاختبار
+test_datagen = ImageDataGenerator(rescale=1./255)  # Pixel Value Normalization for the Test Set
 test_generator = test_datagen.flow_from_directory(
     test_data_dir,
     target_size=image_size,
     batch_size=batch_size,
-    class_mode='sparse')  # تحديد الفئات على أنها sparse
+    class_mode='sparse')  # Defining Categories as Sparse
 
-# تدريب النموذج
+# Model Training
 model.fit(train_generator, epochs=10, validation_data=test_generator)
 
-# تقييم النموذج وحساب الدقة
+# Model Evaluation and Accuracy Calculation
 test_loss, test_accuracy = model.evaluate(test_generator)
 print("Test accuracy: ",test_accuracy * 100,"%")
 
-# تحديد مسار حفظ النموذج كملف .h5
+#Defining the Save Path for the Model as an .h5 File
 model_save_path = 'C:/Users/SHAHAD/Downloads/Telegram Desktop/data-part/modeloldCNNlc.h5'
 
-# حفظ النموذج
+# Save the Model
 model.save(model_save_path)
